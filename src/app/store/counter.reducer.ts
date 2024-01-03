9f140643774e55d3ef8f559706e8837ab7e2287e
BHNB <rjnunes.prestserv@petrobras.com.br>
2024-01-03 09:22:44 -0300

    Criação de uma action

--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -1,11 +1,16 @@
-import { createReducer } from "@ngrx/store";
+import { createReducer, on } from "@ngrx/store";
+
+import { increment } from "./counter.actions";
 
 const initialState = 0;
 
 // Mais nova forma de criação do reducer:
-// export const counterReducer = createReducer(initialState);
+export const counterReducer = createReducer(
+    initialState,
+    on(increment, (state) => state + 1)
+);
 
 // Mais velha forma de criação do reducer:
-export function counterReducer(state = initialState) {
-    return state;
-}
+// export function counterReducer(state = initialState) {
+//     return state;
+// }

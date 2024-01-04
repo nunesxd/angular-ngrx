7b4a59ec5831bcc5b2f86bc46daea2eee7d99325
BHNB <rjnunes.prestserv@petrobras.com.br>
2024-01-04 14:55:53 -0300

    Explorando o selector

--- src/app/counter-output/counter-output.component.ts
+++ src/app/counter-output/counter-output.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Store } from '@ngrx/store';
 
+import { selectCount, selectDoubleCount } from '../store/counter.selectors';
+
 @Component({
   selector: 'app-counter-output',
   templateUrl: './counter-output.component.html',
@@ -9,8 +11,12 @@ import { Store } from '@ngrx/store';
 })
 export class CounterOutputComponent {
   count$: Observable<number>;
+  doubleCount$: Observable<number>;
 
   constructor(private store: Store<{counter: number}>) {
-    this.count$ = store.select('counter');
+    // Abordagem mais simples, obtemos uma cópia:
+    // this.count$ = store.select('counter');
+    this.count$ = store.select(selectCount);
+    this.doubleCount$ = store.select(selectDoubleCount);
   }
 }

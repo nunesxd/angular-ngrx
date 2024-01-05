e27b3dbc5f9276c9bea61e41a60f92844f60ff1a
BHNB <rjnunes.prestserv@petrobras.com.br>
2024-01-05 10:18:33 -0300

    Adicionando um segundo effect

--- src/app/store/counter.effects.ts
+++ src/app/store/counter.effects.ts
@@ -1,13 +1,24 @@
 import { Injectable } from "@angular/core";
 import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { Store } from "@ngrx/store";
-import { tap, withLatestFrom } from "rxjs";
+import { of, switchMap, tap, withLatestFrom } from "rxjs";
 
-import { decrement, increment } from "./counter.actions";
+import { decrement, increment, init, set } from "./counter.actions";
 import { selectCount } from "./counter.selectors";
 
 @Injectable()
 export class CounterEffects {
+    loadCount = createEffect(() => this.actions$.pipe(
+        ofType(init),
+        switchMap(() => {
+            const storedCounter = localStorage.getItem('count');
+            if(storedCounter) {
+                return of(set({value: Number(storedCounter)}));
+            }
+            return of(set({value: 0}));
+        })
+    ));
+
     saveCount = createEffect(() => this.actions$.pipe(
         ofType(increment, decrement),
         withLatestFrom(this.store.select(selectCount)),
--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -1,6 +1,6 @@
 import { createReducer, on } from "@ngrx/store";
 
-import { increment, decrement } from "./counter.actions";
+import { increment, decrement, set } from "./counter.actions";
 
 const initialState = 0;
 
@@ -8,7 +8,8 @@ const initialState = 0;
 export const counterReducer = createReducer(
     initialState,
     on(increment, (state, action) => state + action.value),
-    on(decrement, (state, action) => state - action.value)
+    on(decrement, (state, action) => state - action.value),
+    on(set, (state, action) => action.value)
 );
 
 // Mais velha forma de criação do reducer:
